416cd73be6ebaf587ff4f97e39f5bab7fb66e73e
fadiHB <eng.fadi.hb@gmail.com>
2020-12-01 20:22:56 +0200

    done

---
+++ madlib_cli/madlib_cli.py
@@ -0,0 +1,47 @@
+import re
+
+def greeting ():
+    print ('''
+Welcome ...
+Put words as called for in the spaces and create a story by  create a story like Mad Libs it'sfun.
+What you've created may end up being fantastic, screamingly funny, shocking, silly, crazy or just
+plain dumb.
+It all depends upon the words you've chosen and how they "fit" into the Madlibs story! 
+    ''')
+
+def read_template (file_path):
+    with open(file_path) as f:
+        data_as_string = f.read()
+        return data_as_string
+
+def parse(data_as_string):
+        data_to_change = re.findall(r"\{(.*?)\}",data_as_string)
+        return data_to_change
+
+def generate_list_items (data_to_change ):
+    user_list = []
+    for i in range(len(data_to_change)):
+        item = int(input(f'Enter a {data_to_change[i]}: '))
+        user_list.append(item)
+    return user_list
+
+def merge (list_new_iteam):
+    new_file_path_test = "new_file_test.txt"
+    with open(new_file_path_test,"w+") as new_f:
+        new_f.write('''I the {} and {} {} have {}
+{}'s {} sister and plan to steal her {}
+{}!''')
+    with open(new_file_path_test, 'r+') as f:
+        data_as_string = f.read()
+        data_updated = data_as_string.format(list_new_iteam[0],list_new_iteam[1],list_new_iteam[2],list_new_iteam[3],list_new_iteam[4],list_new_iteam[5],list_new_iteam[6],list_new_iteam[7])
+        return data_updated
+
+if __name__ == "__main__":
+    greeting ()
+    file_path = 'assets/file.txt'
+    new_file_path = 'assets/new_file.txt'
+    data_as_string = read_template(file_path)
+    data_to_change = parse(data_as_string)
+    list_new_iteam = generate_list_items(data_to_change)
+    data_updated = merge(list_new_iteam)
+    print(data_updated)
--- tests/test_madlib_cli.py
+++ tests/test_madlib_cli.py
@@ -1,5 +1,30 @@
 from madlib_cli import __version__
+from madlib_cli.madlib_cli import read_template,parse,merge
 
 
 def test_version():
     assert __version__ == '0.1.0'
+
+
+data_string = '''I the {Adjective} and {Adjective} {A First Name} have {Past Tense Verb}
+{A First Name}'s {Adjective} sister and plan to steal her {Adjective}
+{Plural Noun}!'''
+
+data_to_change = ['Adjective', 'Adjective', 'A First Name', 'Past Tense Verb', 'A First Name', 'Adjective', 'Adjective', 'Plural Noun']
+
+def test_read_template():
+    actual = read_template('assets/file.txt')
+    expected = data_string
+    assert actual == expected
+
+def test_parse():
+    actual = parse(data_string)
+    expected = data_to_change
+    assert actual == expected
+
+def test_merge():
+    actual = merge([1,2,3,4,5,6,7,8])
+    expected = """I the 1 and 2 3 have 4
+5's 6 sister and plan to steal her 7
+8!"""
+    assert actual == expected
