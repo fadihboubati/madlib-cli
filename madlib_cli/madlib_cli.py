682c2848dc582b9d48d01e191baa861cc958973a
fadiHB <eng.fadi.hb@gmail.com>
2021-07-17 10:50:20 +0300

    refactor + more dynamic

--- madlib_cli/madlib_cli.py
+++ madlib_cli/madlib_cli.py
@@ -15,23 +15,30 @@ def read_template (file_path):
         return data_as_string
 
 def parse(data_as_string):
-        data_to_change = re.findall(r"\{(.*?)\}",data_as_string)
-        return data_to_change
+        list_data_to_change = re.findall(r"\{(.*?)\}",data_as_string)
+        return list_data_to_change
 
-def generate_list_items (data_to_change ):
+def generate_template_for_update(temp, list_data_to_change):
+    for i in range(len(list_data_to_change)):
+        temp = temp.replace('{'+list_data_to_change[i]+'}','{}')
+    return temp   
+    
+
+def generate_list_items (list_data_to_change):
     user_list = []
-    for i in range(len(data_to_change)):
-        item =input(f'Enter a {data_to_change[i]}: ')
+    for i in range(len(list_data_to_change)):
+        item =input(f' > Enter a {list_data_to_change[i]}: ')
         user_list.append(item)
     return user_list
 
-def merge (list_new_iteam):
+def merge (list_new_iteam, tamplate_updated):
     new_file_path_test = "new_file_test.txt"
     with open(new_file_path_test, 'w+') as f:
-        template = '''I the {} and {} {} have {}
-{}'s {} sister and plan to steal her {}
-{}!'''
-        data_updated = template.format(list_new_iteam[0],list_new_iteam[1],list_new_iteam[2],list_new_iteam[3],list_new_iteam[4],list_new_iteam[5],list_new_iteam[6],list_new_iteam[7])
+#         template = '''I the {} and {} {} have {}
+# {}'s {} sister and plan to steal her {}
+# {}!'''
+        # data_updated = template.format(list_new_iteam[0],list_new_iteam[1],list_new_iteam[2],list_new_iteam[3],list_new_iteam[4],list_new_iteam[5],list_new_iteam[6],list_new_iteam[7])
+        data_updated = tamplate_updated.format(*list_new_iteam)
         f.write(data_updated)
         return data_updated
 
@@ -39,7 +46,8 @@ if __name__ == "__main__":
     greeting ()
     file_path = 'assets/file.txt'
     data_as_string = read_template(file_path)
-    data_to_change = parse(data_as_string)
-    list_new_iteam = generate_list_items(data_to_change)
-    data_updated = merge(list_new_iteam)
+    list_data_to_change = parse(data_as_string)
+    tamplate_updated = generate_template_for_update(data_as_string, list_data_to_change)
+    list_new_iteam = generate_list_items(list_data_to_change)
+    data_updated = merge(list_new_iteam, tamplate_updated)
     print(data_updated)
--- tests/test_madlib_cli.py
+++ tests/test_madlib_cli.py
@@ -1,5 +1,5 @@
 from madlib_cli import __version__
-from madlib_cli.madlib_cli import read_template,parse,merge
+from madlib_cli.madlib_cli import read_template, parse, merge, generate_template_for_update
 
 
 def test_version():
@@ -10,7 +10,7 @@ data_string = '''I the {Adjective} and {Adjective} {A First Name} have {Past Ten
 {A First Name}'s {Adjective} sister and plan to steal her {Adjective}
 {Plural Noun}!'''
 
-data_to_change = ['Adjective', 'Adjective', 'A First Name', 'Past Tense Verb', 'A First Name', 'Adjective', 'Adjective', 'Plural Noun']
+list_data_to_change = ['Adjective', 'Adjective', 'A First Name', 'Past Tense Verb', 'A First Name', 'Adjective', 'Adjective', 'Plural Noun']
 
 def test_read_template():
     actual = read_template('assets/file.txt')
@@ -19,11 +19,12 @@ def test_read_template():
 
 def test_parse():
     actual = parse(data_string)
-    expected = data_to_change
+    expected = list_data_to_change
     assert actual == expected
 
 def test_merge():
-    actual = merge([1,2,3,4,5,6,7,8])
+    temp = generate_template_for_update(data_string, list_data_to_change)
+    actual = merge([1,2,3,4,5,6,7,8], temp)
     expected = """I the 1 and 2 3 have 4
 5's 6 sister and plan to steal her 7
 8!"""
